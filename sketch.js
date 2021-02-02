27c9c3414205da2af415da90d8aaa73875b726b1
sophialadwiniec <slladwiniec@dons.usfca.edu>
2021-02-02 15:44:57 -0800

    adding a star shape, and three ellipses

--- sketch.js
+++ sketch.js
@@ -7,5 +7,46 @@ function setup() {
 
 // Draw code goes here
 function draw() {
-  background(42,125,92,49);;
+  background(20,112,94,44);
+  // center pink piece on screen shape 1 
+  star(50,50);
+
+  // double ellipse one 
+  fill(216,183,250,98);
+  // x y width and height 
+  ellipse(windowWidth/4,windowHeight/4,50);
+  fill(71,71,128,50);
+  // x y width and height 
+  ellipse(windowWidth/4,windowHeight/4,100,100); 
+
+  // double ellipse two 
+  fill(216,183,250,98);
+  // x y width and height 
+  ellipse(windowWidth/60,windowHeight/4,50);
+  fill(71,71,128,50);
+  // x y width and height 
+  ellipse(windowWidth/60,windowHeight/4,100,100); 
+
+  // double ellipse three
+  fill(216,183,250,98);
+  // x y width and height 
+  ellipse(windowWidth/60 - 350,windowHeight/4,50);
+  fill(71,71,128,50);
+  // x y width and height 
+  ellipse(windowWidth/60 - 350,windowHeight/4,100,100); 
+
+
 }
+
+function star(x1, y1) {
+// moves to a different position on the screen 
+  translate(windowWidth/2, windowHeight/4);
+  noStroke();
+  for (let x = 0; x < 10; x ++) {
+    fill(245,190,241,96);
+    // x y width height 
+    ellipse(x1, y1, 10, 60);
+    rotate(PI/5);
+  }
+}
+
