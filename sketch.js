269f7b2185dee707d96ddadc7da8fed27af02a3c
sophialadwiniec <slladwiniec@dons.usfca.edu>
2021-02-03 13:46:45 -0800

    cleaning up code

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 
 // changing color variables for ellipses 
-var r = 0; 
-var b = 0; 
-var g = 0; 
-var t = 0; 
+var e_r = 0; 
+var e_b = 0; 
+var e_g = 0; 
+var e_t = 0; 
 
 // changing color variables for flowers 
-var r1 = 0; 
-var b1 = 0; 
-var g1 = 0; 
-var t1 = 0; 
+var f_r = 0; 
+var f_b = 0; 
+var f_g = 0; 
+var f_t = 0; 
 
 // changing color variables for background 
 var b_b = 0; 
@@ -34,7 +34,7 @@ function draw() {
       if(x % 2 != 0){
         flower((7/i), (7/x)); 
       }else{
-        drawEllipses((i/7), (x/7))
+        drawEllipse((i/7), (x/7))
       }
     }
   }
@@ -42,13 +42,13 @@ function draw() {
 }
 
 
-// draws 6 total ellipses 
-function drawEllipses(width1, height1) {
-  // double ellipse one 
+// draws 2 total ellipses 
+function drawEllipse(width1, height1) {
+ 
   fill(216,183,250,98);
   // x y width and height 
-  ellipse(width * width1,height * height1,50);
-  fill(r,b,g,t);
+  ellipse(width * width1,height * height1,50, 50);
+  fill(e_r,e_b,e_g,e_t);
   // x y width and height 
   ellipse(width* width1,height * height1,100,100); 
 
@@ -58,15 +58,15 @@ function drawEllipses(width1, height1) {
 // function that makes the page turn pink if you mouse is on the upper half and blue if your mouse is on the lower half
 function mouseMoved(){
   if((mouseY < height/2) && (mouseX < width/2)){
-    r1 = 245;  
-    b1 = 190; 
-    g1 = 241; 
-    t1 = 96;
+    f_r = 245;  
+    f_b = 190; 
+    f_g = 241; 
+    f_t = 96;
 
-    r = 245; 
-    b = 192; 
-    g = 236; 
-    t = 96; 
+    e_r = 245; 
+    e_b = 192; 
+    e_g = 236; 
+    e_t = 96; 
 
     b_b = 250; 
     r_b = 86;
@@ -74,45 +74,45 @@ function mouseMoved(){
     t_b = 98;  
  
   } else if ((mouseY > height/2) && (mouseX > width/2)) {
-    r1 = 52; 
-    b1 = 224; 
-    g1 = 187; 
-    t1 = 88;
+    f_r = 52; 
+    f_b = 224; 
+    f_g = 187; 
+    f_t = 88;
 
-    r = 79; 
-    b = 176; 
-    g = 160; 
-    t = 69; 
+    e_r = 79; 
+    e_b = 176; 
+    e_g = 160; 
+    e_t = 69; 
 
     b_b = 20; 
     r_b =112;
     g_b = 94; 
     t_b = 44;
   } else if ((mouseY < height/2) && (mouseX > width/2)) {
-    r1 = 83; 
-    b1 = 232; 
-    g1 = 145; 
-    t1 = 91;
+    f_r = 83; 
+    f_b = 232; 
+    f_g = 145; 
+    f_t = 91;
 
-    r = 14; 
-    b = 227; 
-    g = 103; 
-    t = 89; 
+    e_r = 14; 
+    e_b = 227; 
+    e_g = 103; 
+    e_t = 89; 
 
     b_b = 9; 
     r_b = 102;
     g_b = 48; 
     t_b = 40;
   } else if ((mouseY > height/2) && (mouseX < width/2)) {
-    r1 = 167; 
-    b1 = 86; 
-    g1 = 245; 
-    t1 = 96;
+    f_r = 167; 
+    f_b = 86; 
+    f_g = 245; 
+    f_t = 96;
 
-    r = 120; 
-    b = 13; 
-    g = 219; 
-    t = 86; 
+    e_r = 120; 
+    e_b = 13; 
+    e_g = 219; 
+    e_t = 86; 
 
     b_b = 46; 
     r_b = 6;
@@ -134,7 +134,7 @@ function flower(width1, height1) {
   translate(width/width1,height/height1); 
   noStroke();
   for (let x = 0; x < 10; x ++) {
-    fill(r1,b1,g1,t1);
+    fill(f_r,f_b,f_g,f_t);
     // x y width height 
     ellipse(10,30, 20, 50); 
     rotate(PI/5);
